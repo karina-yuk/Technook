bafe838187c7bb12194b1f09be94636fef4100a8
unknown <karina.yukting.li@gmail.com>
2024-03-21 00:33:00 -0400

    Updated index.js seeds and added comments

--- seeds/index.js
+++ seeds/index.js
@@ -1,20 +1,32 @@
-const sequelize = require('../config/connection.js');
+// Import the seed functions for users, posts, and comments from their respective files
+const seedUsers = require('./userSeeds');
+const seedPosts = require('./postSeeds');
+const seedComments = require('./commentSeeds');
 
-const postsSeeds = require('./postsSeeds.js');
-const commentsSeeds = require('./commentsSeeds.js');
-const usersSeeds = require('./usersSeeds.js');
+// Import the Sequelize connection instance from the specified path
+const sequelize = require('../config/connection');
 
+// Define a function called seedAll, which is an asynchronous function that seeds the database with users, posts, and comments
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-    await usersSeeds();
-    console.log('\n----- USERS SEEDED -----\n');
-    await postsSeeds();
-    console.log('\n----- POSTS SEEDED -----\n');
-    await commentsSeeds();
-    console.log('\n----- COMMENTS SEEDED -----\n');
+  // Synchronize the Sequelize models with the database and force the creation of tables by setting { force: true }
+  await sequelize.sync({ force: true });
+  console.log('\n\x1b[32m----- DATABASE SYNCED -----\x1b[0m\n');
 
-    process.exit(0);
+  // Seed the database with users using the seedUsers function and log a message indicating completion
+  await seedUsers();
+  console.log('\n\x1b[32m----- USERS SEEDED -----\x1b[0m\n');
+
+  // Seed the database with posts using the seedPosts function and log a message indicating completion
+  await seedPosts();
+  console.log('\n\x1b[32m----- POSTS SEEDED -----\x1b[0m\n');
+
+  // Seed the database with comments using the seedComments function and log a message indicating completion
+  await seedComments();
+  console.log('\n\x1b[32m----- COMMENTS SEEDED -----\x1b[0m\n');
+
+  // Exit the process with a code of 0 (indicating successful execution)
+  process.exit(0);
 };
 
+// Call the seedAll function to initiate the database seeding process
 seedAll();
